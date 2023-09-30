ccefcb35538169dd5d33130fb37eb67903c56578
cartlex <ap26booking@gmail.com>
2023-09-30 12:55:00 +0300

    add skim function test

--- test/Uniswap.t.sol
+++ test/Uniswap.t.sol
@@ -32,7 +32,7 @@ contract UniswapTest is Test, Constants {
         vm.stopPrank();
     }
 
-    function test_user1_Swap() public {
+    function test_swap() public {
         vm.startPrank(user1);
 
         IERC20(DAI).safeIncreaseAllowance(address(uniswap), AMOUNT_IN);
@@ -59,7 +59,7 @@ contract UniswapTest is Test, Constants {
         emit log_named_decimal_uint("WBTC balance after", user1WBTCBalanceAfter, ERC20(WBTC).decimals());
     }
 
-    function test_user1_addLiquidity() public {
+    function test_addLiquidity() public {
         vm.startPrank(user1);
 
         address pair = IUniswapV2Factory(FACTORY).getPair(DAI, WBTC);
@@ -95,20 +95,19 @@ contract UniswapTest is Test, Constants {
         emit log_named_decimal_uint("reserve DAI", _reserve1After, ERC20(DAI).decimals());
     }
 
-    function test_user1_removeLiquidity() public {
+    function test_removeLiquidity() public {
         vm.startPrank(user1);
 
         address pair = IUniswapV2Factory(FACTORY).getPair(DAI, WBTC);
         
         console2.log(StdStyle.magenta("================ Before adding liquidity ================"));
 
-
         {
-        (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
-        emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
-        emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
-        emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
-        emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
         }
 
         IERC20(DAI).safeIncreaseAllowance(address(uniswap), type(uint256).max);
@@ -131,12 +130,12 @@ contract UniswapTest is Test, Constants {
         console2.log(StdStyle.magenta("================ After adding liquidity ================"));
 
         {   
-        (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
-        emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
-        emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
-        emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
-        emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
-        emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
         }
 
         IERC20(pair).safeIncreaseAllowance(address(uniswap), type(uint256).max);
@@ -155,14 +154,94 @@ contract UniswapTest is Test, Constants {
         console2.log(StdStyle.magenta("================ After removing liquidity ================"));
 
         {   
-        emit log_named_decimal_uint("amount of tokenA removed", amountOfTokenARemoved, ERC20(DAI).decimals());
-        emit log_named_decimal_uint("amount of tokenB removed", amountOfTokenBRemoved, ERC20(WBTC).decimals());
-        (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
-        emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
-        emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
-        emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
-        emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
-        emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("amount of tokenA removed", amountOfTokenARemoved, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("amount of tokenB removed", amountOfTokenBRemoved, ERC20(WBTC).decimals());
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+        }
+    }
+
+    function test_skim() public {
+        vm.startPrank(user1);
+
+        address pair = IUniswapV2Factory(FACTORY).getPair(DAI, WBTC);
+        
+        console2.log(StdStyle.magenta("================ Before adding liquidity ================"));
+
+        {
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+        }
+
+        IERC20(DAI).safeIncreaseAllowance(address(uniswap), type(uint256).max);
+        IERC20(WBTC).safeIncreaseAllowance(address(uniswap), type(uint256).max);
+
+        uint256 amountDAIToAdd = 50_000e18;
+        uint256 amountWBTCToAdd = 2e8;
+
+        (,, uint256 liquidity) = uniswap.addLiquidity(
+            DAI,
+            WBTC,
+            amountDAIToAdd,
+            amountWBTCToAdd,
+            user1,
+            block.timestamp + 1
+        );
+
+        emit log_named_decimal_uint("liquidity tokens minted", liquidity, IUniswapV2Pair(pair).decimals());
+
+        console2.log(StdStyle.magenta("================ After adding liquidity and before direct transfer ================"));
+
+        {   
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+        }
+
+        {
+            uint256 user1DAIBalanceBefore = IERC20(DAI).balanceOf(user1);
+            emit log_named_decimal_uint("user1 DAI balance before direct transfer", user1DAIBalanceBefore, ERC20(DAI).decimals());
+        }
+
+        uint256 daiAmountToTransfer = 5_000e18;
+        IERC20(DAI).safeTransfer(pair, daiAmountToTransfer);
+
+        console2.log(StdStyle.magenta("================ After direct transfer ================"));
+
+        {   
+            uint256 user1DAIBalanceAfter = IERC20(DAI).balanceOf(user1);
+            emit log_named_decimal_uint("user1 DAI balance before skim function used", user1DAIBalanceAfter, ERC20(DAI).decimals());
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
+        }
+
+        IUniswapV2Pair(pair).skim(user1);
+
+        console2.log(StdStyle.magenta("================ After sync function used ================"));
+
+        {   
+            uint256 user1DAIBalanceAfter = IERC20(DAI).balanceOf(user1);
+            emit log_named_decimal_uint("user1 DAI balance after skim function used", user1DAIBalanceAfter, ERC20(DAI).decimals());
+            (uint112 _reserve0Before, uint112 _reserve1Before,) = IUniswapV2Pair(pair).getReserves();
+            emit log_named_decimal_uint("user1 LP tokens balance", IUniswapV2Pair(pair).balanceOf(user1), IUniswapV2Pair(pair).decimals());
+            emit log_named_decimal_uint("pool DAI balance", IERC20(DAI).balanceOf(pair), ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC balance", IERC20(WBTC).balanceOf(pair), ERC20(WBTC).decimals());
+            emit log_named_decimal_uint("pool DAI reserves", _reserve1Before, ERC20(DAI).decimals());
+            emit log_named_decimal_uint("pool WBTC reserves", _reserve0Before, ERC20(WBTC).decimals());
         }
     }
 }
